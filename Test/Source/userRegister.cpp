4a483605624c10ae1762201b5deb4bec7bc45a20
Dawid Winiarski <winiu31@gmail.com>
2015-07-14 20:37:14 +0200

    added new test for registering user

--- Test/Source/userRegister.cpp
+++ Test/Source/userRegister.cpp
@@ -2,14 +2,17 @@
 #include "ServerInMessage.hpp"
 #include "ServerOutMessage.hpp"
 #include "tryToRegisterUser.hpp"
-
+#include "sendMessage.hpp"
 
 
 int main(int argc, char* argv[])
 {
     MessageQueuesIds queueIds = initMessageQueues(argc, argv);
-
     tryToRegisterUser("winiarz", "password1", ServerNotRunning, queueIds);
-
+    startServer (queueIds);
+    tryToRegisterUser("winiarz", "password1", RegisterSuccessful, queueIds);
+    tryToRegisterUser("winiarz", "password2", UsernameAlreadyUsed, queueIds);
+    tryToRegisterUser("mieciu", "password3", RegisterSuccessful, queueIds);
+    shutdownServer (queueIds);
     return 0;
 }
